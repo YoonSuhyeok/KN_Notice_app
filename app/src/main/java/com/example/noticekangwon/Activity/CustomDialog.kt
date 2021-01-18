ec364d9526349564c63e1145756ff5b8d020d450
송승재 <pumpkintime123@gmail.com>
2021-01-18 11:06:26 +0900

    Merge remote-tracking branch 'origin/DBupdate1' into DBupdate
    # Conflicts:
    #	app/src/main/java/com/example/noticekangwon/Activity/MainActivity.kt
    #	app/src/main/java/com/example/noticekangwon/DataBase/Notice.java
    #	app/src/main/res/layout/activity_main.xml
    #	app/src/main/res/menu/menu.xml
    #	app/src/main/res/values-night/colors.xml
    #	app/src/main/res/values/colors.xml
    #	app/src/main/res/values/styles.xml

--- app/src/main/java/com/example/noticekangwon/Activity/CustomDialog.kt
+++ app/src/main/java/com/example/noticekangwon/Activity/CustomDialog.kt
@@ -1,5 +1,8 @@
 package com.example.noticekangwon.Activity
 
+import android.app.Activity
+import android.content.Context
+import android.content.SharedPreferences
 import android.graphics.Color
 import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
@@ -8,13 +11,21 @@ import android.view.View
 import android.view.ViewGroup
 import android.view.Window
 import android.widget.Button
+import android.widget.RadioGroup
 import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.DialogFragment
 import com.example.noticekangwon.R
+import com.example.noticekangwon.defaultClass.ThemeSet
+import kotlinx.android.synthetic.main.dialog_custom.*
+import kotlinx.android.synthetic.main.dialog_custom.radioBtn1
+import kotlinx.android.synthetic.main.dialog_custom.rdG1
+import kotlinx.android.synthetic.main.dialog_custom.view.*
 
 class CustomDialog : DialogFragment() {
 
-    var title: String? = null
+    var title: String? = "모드 설정"
+    var con: Context? = null
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -32,15 +43,50 @@ class CustomDialog : DialogFragment() {
         super.onActivityCreated(savedInstanceState)
         view?.apply {
             findViewById<TextView>(R.id.text_title)?.text = title
+
+            var shared: SharedPreferences? = con?.getSharedPreferences("themeSetSP", 0)
+            var edit = shared?.edit()
+            var mode = shared?.getString("themeMode", ThemeSet.LIGHT_MODE)
+
+            when(mode) {
+                ThemeSet.LIGHT_MODE -> radioBtn1.isChecked = true
+                ThemeSet.DARK_MODE -> radioBtn2.isChecked = true
+                ThemeSet.DEFAULT_MODE -> radioBtn3.isChecked = true
+            }
+
+            rdG1.setOnCheckedChangeListener { _, checkId ->
+                when (checkId) {
+                    R.id.radioBtn1 -> {
+                        edit?.clear()
+                        edit?.putString("themeMode", ThemeSet.LIGHT_MODE)
+                        ThemeSet.applyTheme(ThemeSet.LIGHT_MODE)
+                        edit?.commit()
+                    }
+                    R.id.radioBtn2 -> {
+                        edit?.clear()
+                        edit?.putString("themeMode", ThemeSet.DARK_MODE)
+                        ThemeSet.applyTheme(ThemeSet.DARK_MODE)
+                        edit?.commit()
+                    }
+                    R.id.radioBtn3 -> {
+                        edit?.clear()
+                        edit?.putString("themeMode", ThemeSet.DEFAULT_MODE)
+                        ThemeSet.applyTheme(ThemeSet.DEFAULT_MODE)
+                        edit?.commit()
+                    }
+                }
+            }
         }
     }
+
 
     class CustomDialogBuilder {
 
         private val dialog = CustomDialog()
 
-        fun setTitle(title: String): CustomDialogBuilder {
+        fun setTitle(title: String, context: Context): CustomDialogBuilder {
             dialog.title = title
+            dialog.con = context
             return this
         }
 
